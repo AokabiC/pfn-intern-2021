204c240841cff600d37ec51bb611ca1098932fef
AokabiC <syht0312@gmail.com>
2021-05-09 08:06:57 +0900

    feat: show uploaded image

--- src/FileInput.tsx
+++ src/FileInput.tsx
@@ -1,20 +1,44 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
+
+interface ImgData {
+  file: File | null
+  name: string
+  imagePreviewUrl: string
+}
 
 export const FileInput: React.FC = () => {
   const fileInput = useRef<HTMLInputElement>(null)
+  const [uploadedImg, setUploadedImg] = useState<ImgData>({
+    file: null,
+    name: '',
+    imagePreviewUrl: '',
+  })
   const handleSubmit: React.FormEventHandler = (event) => {
     event.preventDefault()
-    alert(`Selected file - ${fileInput.current?.files?.item(0)?.name}`)
+    let reader = new FileReader()
+    const uploadedImgFile = fileInput.current?.files?.item(0)
+    reader.onloadend = () => {
+      setUploadedImg({
+        file: uploadedImgFile ?? null,
+        name: uploadedImgFile?.name ?? '',
+        imagePreviewUrl: reader.result?.toString() ?? '',
+      })
+    }
+    if (uploadedImgFile != null) {
+      reader.readAsDataURL(uploadedImgFile)
+    }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Upload file:
-        <input type="file" ref={fileInput} />
-      </label>
-      <br />
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Upload file:
+          <input type="file" ref={fileInput} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <img src={uploadedImg.imagePreviewUrl} alt={uploadedImg.name} />
+    </>
   )
 }
